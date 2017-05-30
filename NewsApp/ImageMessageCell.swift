15d3ee8a6645dc1550649dc6fb2b629b4649ed0e
Naveen Kumar Kanumoori <naveenkanumoori@gmail.com>
2017-05-30 01:25:33 -0400

    New Cells added

---
+++ NewsApp/ImageMessageCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageMessageCell.swift
+//  NewsApp
+//
+//  Created by Naveen Kumar on 5/30/17.
+//  Copyright © 2017 Naveen Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ImageMessageCell: UITableViewCell {
+
+    @IBOutlet weak var dataView: UIView!
+    @IBOutlet weak var imageMessage: UIImageView!
+    @IBOutlet weak var senderName: UILabel!
+    @IBOutlet weak var createdTime: UILabel!
+    
+}
---
+++ NewsApp/TextMessageCell.swift
@@ -0,0 +1,21 @@
+//
+//  TextMessageCell.swift
+//  NewsApp
+//
+//  Created by Naveen Kumar on 5/30/17.
+//  Copyright © 2017 Naveen Kumar. All rights reserved.
+//
+
+import UIKit
+
+class TextMessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageText: UILabel!
+    
+    @IBOutlet weak var senderName: UILabel!
+    
+    @IBOutlet weak var createdTime: UILabel!
+    
+    @IBOutlet weak var dataView: UIView!
+    
+}
